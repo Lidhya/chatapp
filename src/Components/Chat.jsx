827b90c9187204b9d3e382bed42d07f72ce7ff3c
Lidhya <lidhyajeejan@gmail.com>
2023-04-19 01:32:04 +0530

    Chat added

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-
+import Messages from './Messages'; 
+import Input from './Input'; 
+import VideocamIcon from '@mui/icons-material/Videocam';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 const Chat = () => {
     return (
         <div className='chat'>
-            jkbib
+            <div className="chatInfo">
+        <span>user name</span>
+        <div className="chatIcons">
+          <VideocamIcon/>
+          <PersonAddIcon/>
+          <MoreVertIcon />
+        </div>
+      </div>
+      <Messages/>
+      <Input/>
         </div>
     );
 };
