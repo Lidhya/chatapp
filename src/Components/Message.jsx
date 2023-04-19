23a75615a2c583993ff8592daaa84903eda983fa
Lidhya <lidhyajeejan@gmail.com>
2023-04-20 02:15:48 +0530

    Message conent added

--- src/Components/Message.jsx
+++ src/Components/Message.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import user1 from "../assests/pexels-gilberto-olimpio-3686216.jpg"
 
 const Message = () => {
     return (
-        <div className='message'>
-          afasdfsdsdg  
+      <div className="message">
+        <div className="messageInfo">
+        <img src={user1} alt="user" />
+        <span>just now</span>
+        </div>
+        <div className='messageContent'>
+          <span> afasdfsdsdg </span> 
+          <img src={user1} alt="user" />
+        </div>
         </div>
     );
 };
