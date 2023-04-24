31663d5ad6e38f1e85b9c76c6b12057d0f96a8e6
Lidhya <lidhyajeejan@gmail.com>
2023-04-25 03:24:50 +0530

    React router dom setup complete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import Register from "./Pages/Register";
 import Login from "./Pages/Login";
 import Home from "./Pages/Home";
+import {
+ BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
 import "./style.scss";
 
 function App() {
   return (
-    <div className="App">
-      <Register></Register>
-      {/* <Login></Login> */}
-      {/* <Home/> */}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home/>}></Route>
+          <Route path="register" element={<Register/>}></Route>
+          <Route path="login" element={<Login/>}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
