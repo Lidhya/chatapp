f4eb2ace3d9564a10453fcf15175899e949c7601
Lidhya <lidhyajeejan@gmail.com>
2023-04-27 01:45:14 +0530

    displayName and Photo added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import user1 from "../assests/pexels-gilberto-olimpio-3686216.jpg"
 import { signOut } from 'firebase/auth';
 import { auth } from '../firebase';
+import { AuthContext } from '../Context/AuthContext';
 
 
 const Navbar = () => {
+    const { currentUser}=useContext(AuthContext)
     const handleLogout= () => signOut(auth) 
     return (
         <div className='navbar'>
             <h4>MyChatApp</h4>
             <div className='user'>
-            <img src={user1} alt="user" />
-            <p>user</p>
+            <img src={currentUser.photoURL} alt="user" />
+            <p>{currentUser.displayName}</p>
             <button onClick={handleLogout}>Logout</button>
             </div>
         </div>
