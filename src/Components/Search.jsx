cab111c613ae231f6aa674946a84404b40a5d090
Lidhya <lidhyajeejan@gmail.com>
2023-04-28 01:03:01 +0530

    Search completed

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,15 +1,31 @@
-import React, { useEffect, useState } from "react";
-import { where, collection, query, getDocs } from "firebase/firestore";
+import React, { useContext, useEffect, useState } from "react";
+import {
+  where,
+  collection,
+  query,
+  getDocs,
+  getDoc,
+  doc,
+  setDoc,
+  updateDoc,
+  serverTimestamp,
+} from "firebase/firestore";
 import { db } from "../firebase";
+import { AuthContext } from "../Context/AuthContext";
 
 const Search = () => {
   const [userName, setUserName] = useState("");
   const [user, setUser] = useState(null);
-  const [err, setErr] = useState(false);
+  const [err, setErr] = useState(null);
+  const { currentUser } = useContext(AuthContext);
+  const errMessage = "something went wrong"
 
-  useEffect(()=>{
-    userName === "" && setUser(null)
-  }, [userName])
+  useEffect(() => {
+    if (userName === "") {
+      setUser(null);
+      setErr(null);
+    }
+  }, [userName]);
 
   const handleSearch = async () => {
     try {
@@ -19,23 +35,75 @@ const Search = () => {
       );
       getDocs(q)
         .then((querySnapshot) => {
+          if (querySnapshot.empty) {
+            return setErr("User not found");
+          }
           querySnapshot.forEach((doc) => {
             setUser(doc.data());
           });
         })
         .catch((error) => {
-          setErr("User not found");
+          setErr(errMessage);
           console.log(error.message);
         });
     } catch (error) {
       console.log(error.message);
-      setErr(true);
+      setErr(errMessage);
     }
   };
 
   const handleKey = (e) => {
     e.code === "Enter" && handleSearch();
   };
+
+  const handleSelect = () => {
+    const combinedId =
+      currentUser.uid > user.uid
+        ? currentUser.uid + user.uid
+        : user.uid + currentUser.uid;
+    try {
+      getDoc(doc(db, "chats", combinedId))
+        .then((res) => {
+          if (!res.exists()) {
+            setDoc(doc(db, "chats", combinedId), { messages: [] })
+              .then(async () => {
+                await updateDoc(doc(db, "userChat", currentUser.uid), {
+                  [combinedId + "userInfo"]: {
+                    uid: user.uid,
+                    displayName: user.displayName,
+                    photoURL: user.photoURL,
+                  },
+                  [combinedId + ".date"]: serverTimestamp(),
+                });
+
+                await updateDoc(doc(db, "userChat", user.uid), {
+                  [combinedId + "userInfo"]: {
+                    uid: currentUser.uid,
+                    displayName: currentUser.displayName,
+                    photoURL: currentUser.photoURL,
+                  },
+                  [combinedId + ".date"]: serverTimestamp(),
+                });
+              })
+              .catch((error) => {
+                console.log(error.message);
+                setErr(errMessage);
+              });
+          }
+        })
+        .catch((error) => {
+          console.log(error.message);
+          setErr(errMessage);
+        });
+    } catch (error) {
+      console.log(error.message);
+      setErr(errMessage);
+    }
+
+    setUser(null);
+    setUserName("");
+  };
+
   return (
     <div className="search">
       <div className="searchForm">
@@ -47,9 +115,9 @@ const Search = () => {
           value={userName}
         />
       </div>
-      {err && <p>{err}</p>}
+      {err && <p className="userChatInfo">{err}</p>}
       {user && (
-        <div className="userChat">
+        <div className="userChat" onClick={handleSelect}>
           <img src={user.photoURL} alt="" />
           <div className="userChatInfo">
             <span>{user.displayName}</span>
