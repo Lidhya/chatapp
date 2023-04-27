2ee1b58f32fcea080b038e9ca70608bc6136e0f2
Lidhya <lidhyajeejan@gmail.com>
2023-04-28 02:39:26 +0530

    Wrapepd with chatcontext

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { AuthContextProvider } from './Context/AuthContext';
 import './index.css';
 import App from './App';
+import { ChatContextProvider } from './Context/ChatContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <AuthContextProvider>
-  {/* <React.StrictMode> */}
+    <ChatContextProvider>
+  <React.StrictMode>
     <App />
-  {/* </React.StrictMode> */}
+  </React.StrictMode>
+  </ChatContextProvider>
   </AuthContextProvider>
 );
 
