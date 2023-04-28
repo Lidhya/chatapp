dcf1dedf159a5b62ccd518f8abd621f6474d19b0
Lidhya <lidhyajeejan@gmail.com>
2023-04-28 14:42:59 +0530

    Messages added

--- src/Components/Messages.jsx
+++ src/Components/Messages.jsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import Message from './Message';
-
+import React, { useContext, useEffect, useState } from "react";
+import Message from "./Message";
+import { doc, onSnapshot } from "firebase/firestore";
+import { db } from "../firebase";
+import { ChatContext } from "../Context/ChatContext";
 
 const Messages = () => {
-    return (
-        <div className='messages'>
-            <Message classname={"owner"}/> 
-            <Message/> 
-            <Message/> 
-            <Message/> 
-            <Message/> 
-            <Message/> 
-            <Message classname={"owner"}/> 
-            <Message/> 
-            <Message/> 
-            <Message/> 
-            <Message classname={"owner"}/> 
-            <Message/> 
-            <Message/> 
-            <Message/> 
-        </div>
-    );
-}
+  const [messages, setMessages] = useState([]);
+  const { data } = useContext(ChatContext);
+
+ useEffect(() => {
+   
+    if (data.chatId) {
+       const unSub = onSnapshot(doc(db, "chats", data?.chatId), (doc) => {
+         doc.exists() && setMessages(doc.data().messages);
+       });
+   
+       return () => {
+         unSub();
+       };
+    } 
+  }, [data]);
+
+  return (
+    <div className="messages">
+      {messages[0]? messages.map((m) => (
+        <Message message={m} key={m.id} />
+      )):
+     <div> <p>Start a converstation</p></div>
+      }
+    </div>
+  );
+};
 
 export default Messages;
